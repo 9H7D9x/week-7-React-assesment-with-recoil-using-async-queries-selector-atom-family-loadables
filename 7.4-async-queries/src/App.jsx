832217bac8e83c78fce1cd6bf729ae843075656a
Harshit_Dahiya <dahiya.harshit12@gmail.com>
2024-03-24 16:32:51 +0530

    async-data-queries done

--- 7.4-async-queries/src/App.jsx
+++ 7.4-async-queries/src/App.jsx
@@ -15,13 +15,13 @@ function MainApp() {
   const [networkCount, setNetworkCount] = useRecoilState(notifications)
   const totalNotificationCount = useRecoilValue(totalNotificationSelector);
 
-  useEffect(() => {
-    // fetch
-    axios.get("https://sum-server.100xdevs.com/notifications")
-      .then(res => {
-        setNetworkCount(res.data)
-      })
-  }, [])
+  // useEffect(() => {
+  //   // fetch
+  //   axios.get("https://sum-server.100xdevs.com/notifications")
+  //     .then(res => {
+  //       setNetworkCount(res.data)
+  //     })
+  // }, [])
 
   return (
     <>
